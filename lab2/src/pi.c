196c1afa9bcf5ab150f367ce134c0a84b15ee20d
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2022-03-22 20:15:35 +0100

    implemented pi.c

--- lab2/src/pi.c
+++ lab2/src/pi.c
@@ -3,12 +3,12 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <float.h>
 #define TIME_SEED time(NULL)
 
-double compute_pi(long int n) {
-    long int i, count;
-    double x, y, z, pi;
-
+long int compute_points_within(long int n) {
+    long long int i, count;
+    double x, y, z;
     count = 0;
     for(i = 0; i < n; ++i) {
         x = (double)rand() / RAND_MAX;
@@ -19,35 +19,40 @@ double compute_pi(long int n) {
         }
     }
 
-    pi = (double) count / n * 4;
-    return pi;
+    return count;
 }
 
 /*
     Useful mpi functions:
-        - Scatter - divides data ( we have no data to scatter)
+        - Scatter - divides data (we have almost no data to scatter)
         - Gather - gathers data to  (we don't need to gather ourselves)
-        - Reduce - sums all the data. (we need master which reduces the data)
+        - Reduce - sums all the data. (we need master which will hold the reduced data)
 */
 
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
 
-    int rank, numprocs, seed;
+    int rank, n_nodes, seed;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    seed = TIME_SEED + rank * numprocs;
+    MPI_Comm_size(MPI_COMM_WORLD, &n_nodes);
+    seed = TIME_SEED + rank * n_nodes;
     srand(seed);
-    printf("node_info: { rank: %d, size: %d, seed: %d }\n", rank, numprocs, seed);
 
-    long int n_points;
-    n_points = strtol(argv[1], NULL, 10);
+    long long int n_points;
+    n_points = strtoll(argv[1], NULL, 10);
 
-    printf("n_points: %li\n", n_points);
-    double computed_pi = compute_pi(n_points);
-    printf("computed_pi: %f\n", computed_pi);
+    MPI_Barrier(MPI_COMM_WORLD);
+    double start_time = MPI_Wtime();
+    long long int local_n_points = n_points / n_nodes;
+    long long int local_count = compute_points_within(local_n_points);
+    long long int total_count;
+    MPI_Reduce(&local_count, &total_count, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        double pi = (((double) total_count) / n_points) * 4;
+        double time = MPI_Wtime() - start_time;
+        printf("%d,%.*f,%.*f,%lld,%lld\n", n_nodes, DBL_DECIMAL_DIG, pi, DBL_DECIMAL_DIG, time, n_points, total_count);
+    }
 
     MPI_Finalize();
-
     return 0;
 }
