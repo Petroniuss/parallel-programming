a88f8e8784fdd34c817c07cc0ce31e0c9831e69f
Patryk Wojtyczek <plgpwojtyczek@login02.prometheus.cyfronet.pl>
2022-03-23 21:05:54 +0100

    more measurements

---
+++ lab2/run-big.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --nodes 1
+#SBATCH --ntasks 12
+#SBATCH --time=01:00:00
+#SBATCH --partition=plgrid-short
+#SBATCH --account=plgmpr22
+#SBATCH --sockets-per-node=2
+
+module add plgrid/tools/openmpi
+make
+
+for repeat in {1..20}; do
+	for points in 20000000000; do
+		for nodes in {1..12}; do
+			mpiexec -np $nodes ./build/pi $points | tee -a data/data.csv
+		done
+	done
+done
+
